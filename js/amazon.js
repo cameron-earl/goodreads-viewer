e1aa203d20cb823075184fcb165afa4421d1e6c9
Cameron Earl <developer.cameron@gmail.com>
2017-09-22 02:03:46 -0700

    Pretty up amazon.js for sharing

--- js/amazon.js
+++ js/amazon.js
@@ -1,8 +1,11 @@
-//Keys must be initialized for this to function.
+// Methods useful for interacting with the Amazon Product Advertising API
+// https://docs.aws.amazon.com/AWSECommerceService/latest/DG/rest-signature.html
+// Keys must be initialized for this to function.
 let AMAZON_PUBLIC_KEY;
 let AMAZON_PRIVATE_KEY;
 let AMAZON_TAG;
 
+//Finds the price of an book described by the object (ISBN, Title, Author)
 function getAmazonPrice(argsObj, callback) {
   let asin;
   let args = objToArgString(argsObj);
@@ -20,6 +23,7 @@ function getAmazonPrice(argsObj, callback) {
   });
 }
 
+//Takes book information (ISBN, Title, Author) and creates part of a query
 function objToArgString(argsObj) {
   let args = [];
   if (argsObj.author && argsObj.author.length) {
@@ -39,15 +43,15 @@ function objToArgString(argsObj) {
   return (args);
 }
 
+//Encodes more characters than the standard encodeURIComponent
 function fixedEncodeURIComponent(str) {
-  return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
+  return encodeURIComponent(str).replace(/[!'()]/g, function(c) {
     return '%' + c.charCodeAt(0).toString(16);
-  });
+  }).replace(/\*/g,''); //If there's an asterisk, it will fail, encoded or not
 }
 
-//example: makeAmazonRequest('ItemSearch','Keywords=the%20hunger%20games&ISBN=0439023483');
+
 function makeAmazonRequest(operation, customArgs, callback) {
-  // https://docs.aws.amazon.com/AWSECommerceService/latest/DG/rest-signature.html
   let base = 'http://webservices.amazon.com/onca/xml?';
   let args = generateQueryArgs(operation, customArgs);
   let signature = '&Signature=' + getSignature(args, AMAZON_PRIVATE_KEY);
@@ -55,13 +59,14 @@ function makeAmazonRequest(operation, customArgs, callback) {
   completeAmazonRequest(url, callback);
 }
 
+// If error due to throttling, keeps calling until successful
 function completeAmazonRequest(url, callback) {
   $.get(url)
     .done(callback)
     .fail((e) => {
       if (e.status === 503) {
-        setTimeout(completeAmazonRequest(url, callback), (500 + Math.floor(Math.random() * 1000)));
-        return true
+        delay = Math.floor(Math.random() * 1000) + 500; //0.5-1.5sec
+        setTimeout(completeAmazonRequest(url, callback), delay);
       } else if (e.status === 403) {
         console.log("Malformed request!" + url);
         console.log(e);
@@ -72,6 +77,7 @@ function completeAmazonRequest(url, callback) {
     });
 }
 
+//Pretties up the query string for the signature hash
 function generateQueryArgs(operation, customArgs) {
   let standardArgs = `Service=AWSECommerceService&AWSAccessKeyId=${AMAZON_PUBLIC_KEY}&Version=2013-08-01&AssociateTag=${AMAZON_TAG}`;
   //They don't allow price search on kindle books! SearchIndex=KindleStore :(
@@ -82,10 +88,11 @@ function generateQueryArgs(operation, customArgs) {
   args += '&Timestamp=' + timestamp;
   args = args.split('&');
   args = args.map(a => a.split('=')).map(o => o[0] + '=' + fixedEncodeURIComponent(o[1]));
-  args = args.sort().join('&').replace(/'/g, '%27');
+  args = args.sort().join('&');
   return args;
 }
 
+// npm js-sha256 package
 function getSignature(argStr, key) {
   let prepend =
     `GET
@@ -102,8 +109,9 @@ webservices.amazon.com
   return encodeURIComponent(hash);
 }
 
-function hexToBase64(hexstring) {
-  return btoa(hexstring.match(/\w{2}/g).map(function(a) {
-    return String.fromCharCode(parseInt(a, 16));
-  }).join(""));
+//converts base 16 string to base 64
+function hexToBase64(hexStr) {
+  return btoa(hexStr.match(/\w{2}/g)
+    .map(a=>String.fromCharCode(parseInt(a,16)))
+    .join(""));
 }
